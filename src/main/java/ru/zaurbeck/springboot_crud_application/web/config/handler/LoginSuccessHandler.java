efc609a6f45ccccba3e82391bdfeb015bec684fb
ShaipovZA <z.shaipov@id-mt.ru>
2021-01-23 21:16:40 +0300

    new branch "bootstrap", 3.1.2 done

--- src/main/java/ru/zaurbeck/springboot_crud_application/web/config/handler/LoginSuccessHandler.java
+++ src/main/java/ru/zaurbeck/springboot_crud_application/web/config/handler/LoginSuccessHandler.java
@@ -2,42 +2,36 @@ package ru.zaurbeck.springboot_crud_application.web.config.handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import ru.zaurbeck.springboot_crud_application.web.model.User;
 import ru.zaurbeck.springboot_crud_application.web.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Set;
 
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
+    private final UserService userService;
+
     @Autowired
-    UserService userService;
+    public LoginSuccessHandler(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
                                         Authentication authentication) throws IOException {
-        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         String userName = null;
         if (principal instanceof UserDetails) {
             userName = ((UserDetails) principal).getUsername();
         }
-        User user = userService.getUserByName(userName);
-
-        if (roles.contains("ROLE_ADMIN")) {
-            httpServletResponse.sendRedirect("/");
-        } else {
-            httpServletResponse.sendRedirect("/user/userpage/" + user.getId());
-        }
+        httpServletResponse.sendRedirect("/user/userpage/" + userService.getUserByName(userName).getId());
     }
 }
